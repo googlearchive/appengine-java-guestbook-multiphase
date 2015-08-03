eab99b65043575b5122f06c56d0b996d3781518a
Les Vogel <lesv@google.com>
2015-08-03 16:36:06 -0700

    Fix comments.
    1. Improve the language
    2. Get rid of pointless servlet
    3. Add comments to SignGuestbookServlet

--- final/src/main/java/com/example/guestbook/SignGuestbookServlet.java
+++ final/src/main/java/com/example/guestbook/SignGuestbookServlet.java
@@ -35,13 +35,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.googlecode.objectify.ObjectifyService;
 
+/**
+ * Form Handling Servlet
+ * Most of the action for this sample is in webapp/guestbook.jsp, which displays the
+ * {@link Greeting}'s. This servlet has one method
+ * {@link #doPost(<#HttpServletRequest req#>, <#HttpServletResponse resp#>)} which takes the form
+ * data and saves it.
+ */
 public class SignGuestbookServlet extends HttpServlet {
+
+  // Process the http POST of the form
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Greeting greeting;
 
     UserService userService = UserServiceFactory.getUserService();
-    User user = userService.getCurrentUser();
+    User user = userService.getCurrentUser();  // Find out who the user is.
 
     String guestbookName = req.getParameter("guestbookName");
     String content = req.getParameter("content");
